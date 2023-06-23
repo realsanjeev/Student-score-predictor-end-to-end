09d0670c59044c6da37dd7a6339a1f08e2ec734b
realsanjeev <realsanjeev2@gmail.com>
2023-06-23 15:34:46 +0545

    Extending logger module to display log in console

--- src/logger.py
+++ src/logger.py
@@ -13,6 +13,15 @@ logging.basicConfig(
     format="[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger().addHandler(console)
 
 def log_message(msg):
     logging.info(msg=msg)
